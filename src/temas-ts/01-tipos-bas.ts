2f395849750a8f57ad66e1662a92729f8fa1602f
Fabian Axel Yañez Santa Cruz <faxyasan86@gmail.com>
2024-09-09 21:03:14 -0600

    primer commit de intro-ts lunes 9

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,25 @@
+
+//console.log ("Hola mundo")
+
+var nombre = 'Juan' 
+let apellido = 'Perez'
+const edad = 30
+
+let num1=2
+let num2='yeyeye'
+let num3=3
+
+let suma = num1 + num2 + num3
+console.log(suma)
+
+let correo: string;
+let edad2:number;
+let activo:boolean;
+
+correo='x@gmail.com'
+edad2=12
+activo=true
+
+let matricula:number|string;
+matricula=12345
+matricula='23AD45'
